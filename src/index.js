30cfe3c6f82a8e6bdd05a96f423543f5de1e53a7
Jaid <jaid.jsx@gmail.com>
2019-02-07 10:29:27 +0100

    Redesigned tldw API (now using xml) and cleaned up
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,78 +12,96 @@
  * @property {object|string} [pkg={}] Path to package.json or pkg data
  */
 
-import {AsyncParallelHook, AsyncSeriesHook} from "tapable"
+import path from "path"
+
 import resolvePkgOption from "resolve-pkg-option"
 
+import xml2js from "xml2js"
 import compile from "./compile"
 
-/**
- * Compiles a markdown text based on given plugins
- * @param {tldwOptions} options
- * @returns {object}
- */
 export default async options => {
   options = {
+    xml: "",
     plugins: [],
     pkg: {},
     ...options,
   }
 
-  options.plugins = options.plugins.map(plugin => {
-    if (typeof plugin === "string" || typeof plugin === "function") {
-      plugin = [plugin]
-    }
-    const [module, pluginOptions = null] = plugin
-    if (typeof module === "function") {
-      return {
-        module,
-        options: pluginOptions,
-      }
-    }
-    const requiredModule = require(prependIf(module, "tldw-plugin-"))
-    return {
-      name: module,
-      module: requiredModule,
-      options: pluginOptions,
-    }
-  })
-
-  const textBlocks = []
-  const write = block => {
-    textBlocks.push(block)
-  }
-
-  const compiler = {
-    options,
-    hooks: {
-      pluginsLoaded: new AsyncParallelHook(["plugins"]),
-      compile: new AsyncSeriesHook(["write"]),
-      compilationDone: new AsyncSeriesHook(["blocks"]),
-      pkgResolved: new AsyncParallelHook(["resolvedPkg"]),
-    },
-  }
-
-  options.plugins.forEach(({module}, index) => {
-    const pluginResponse = module(compiler, options, index)
-    if (typeof pluginResponse === "string") {
-      compiler.hooks.compile.tap(`autocompile-${index}`, writeResponse => {
-        writeResponse(pluginResponse)
-      })
-    }
-  })
-
-  await compiler.hooks.pluginsLoaded.promise(options.plugins)
-
-  if (compiler.hooks.pkgResolved.isUsed()) {
-    const loadedPackage = await resolvePkgOption()
-    compiler.hooks.pkgResolved.promise(loadedPackage)
-  }
-
-  await compiler.hooks.compile.promise(write)
-  const text = compile(textBlocks)
-  await compiler.hooks.compilationDone.promise(text)
+  const loadedPackage = await resolvePkgOption(options.pkg)
 
   return {
-    text,
+    text: options.xml,
   }
 }
+
+// /**
+//  * Compiles a markdown text based on given plugins
+//  * @param {tldwOptions} options
+//  * @returns {object}
+//  */
+// export default async options => {
+//   options = {
+//     xml: null,
+//     plugins: [],
+//     pkg: {},
+//     ...options,
+//   }
+
+//   options.plugins = options.plugins.map(plugin => {
+//     if (typeof plugin === "string" || typeof plugin === "function") {
+//       plugin = [plugin]
+//     }
+//     const [module, pluginOptions = null] = plugin
+//     if (typeof module === "function") {
+//       return {
+//         module,
+//         options: pluginOptions,
+//       }
+//     }
+//     const requiredModule = require(prependIf(module, "tldw-plugin-"))
+//     return {
+//       name: module,
+//       module: requiredModule,
+//       options: pluginOptions,
+//     }
+//   })
+
+//   const textBlocks = []
+//   const write = block => {
+//     textBlocks.push(block)
+//   }
+
+//   const compiler = {
+//     options,
+//     hooks: {
+//       pluginsLoaded: new AsyncParallelHook(["plugins"]),
+//       compile: new AsyncSeriesHook(["write"]),
+//       compilationDone: new AsyncSeriesHook(["blocks"]),
+//       pkgResolved: new AsyncParallelHook(["resolvedPkg"]),
+//     },
+//   }
+
+//   options.plugins.forEach(({module}, index) => {
+//     const pluginResponse = module(compiler, options, index)
+//     if (typeof pluginResponse === "string") {
+//       compiler.hooks.compile.tap(`autocompile-${index}`, writeResponse => {
+//         writeResponse(pluginResponse)
+//       })
+//     }
+//   })
+
+//   await compiler.hooks.pluginsLoaded.promise(options.plugins)
+
+//   if (compiler.hooks.pkgResolved.isUsed()) {
+//     const loadedPackage = await resolvePkgOption()
+//     compiler.hooks.pkgResolved.promise(loadedPackage)
+//   }
+
+//   await compiler.hooks.compile.promise(write)
+//   const text = compile(textBlocks)
+//   await compiler.hooks.compilationDone.promise(text)
+
+//   return {
+//     text,
+//   }
+// }
--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,27 @@
 import path from "path"
 
-import fs from "@absolunet/fss"
+import fss from "@absolunet/fss"
+import fsp from "@absolunet/fsp"
 
 import tldw from "../src"
 import tldwLicense from "../plugins/tldw-plugin-license"
 import tldwInstall from "../plugins/tldw-plugin-install"
 
-it("should run", () => {
-  const result = tldw({
-    plugins: [
-      tldwInstall,
-      tldwLicense,
-    ],
-  })
-  fs.outputFile(path.join(__dirname, "output", "readme.md"), result.text)
-})
+for (const entry of fss.readdir(__dirname)) {
+  if (fss.stat(path.join(__dirname, entry)).isDirectory()) {
+    const testFolder = path.resolve(__dirname, entry)
+    it(entry, async () => {
+      const xml = await fsp.readFile(path.join(testFolder, "tldw.xml"), "utf8")
+      const result = await tldw({
+        xml,
+      })
+      await fsp.outputFile(path.join(testFolder, "dist", "readme.md"), result.text)
+      const expectedFile = path.join(testFolder, "expected.md")
+      const expectedFileExists = await fsp.pathExists(expectedFile)
+      if (expectedFileExists) {
+        const expectedText = await fsp.readFile(expectedFile, "utf8")
+        expect(result.text.trim()).toBe(expectedText.trim())
+      }
+    })
+  }
+}
