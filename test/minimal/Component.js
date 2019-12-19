1a3f7180df2446cffb3c7390bb5ea7f58ebab9cc
Jaid <jaid.jsx@gmail.com>
2019-12-19 20:20:43 +0100

    Replaced more boilerplate code
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/minimal/Component.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default class Content extends React.Component {
-
-  render() {
-    return "1"
-  }
-
-}
--- test/test.js
+++ test/test.js
@@ -1,24 +1,12 @@
+import coffee from "coffee"
 import path from "path"
 
-import fss from "@absolunet/fss"
-import fsp from "@absolunet/fsp"
-import React from "react"
+const main = path.resolve(process.env.MAIN)
 
-import tldw from "../src"
-
-const job = async name => {
-  const testFolder = path.resolve(__dirname, name)
-  const ContentComponent = require(path.join(testFolder, "Component.js")).default
-  const result = await tldw(ContentComponent)
-  await fsp.outputFile(path.join(testFolder, "dist", "readme.md"), result.text)
-  const expectedFile = path.join(testFolder, "expected.md")
-  const expectedFileExists = await fsp.pathExists(expectedFile)
-  if (expectedFileExists) {
-    const expectedText = await fsp.readFile(expectedFile, "utf8")
-    expect(result.text).toBe(expectedText.trim())
-  }
-}
-
-it("basic", () => job("basic"))
-
-it("minimal", () => job("minimal"))
+it("should run", async () => {
+  const outputFile = path.join(__dirname, "..", "dist", "test", "readme.md")
+  return coffee.fork(main, ["output-file", outputFile])
+    .expect("code", 0)
+    .debug()
+    .end()
+})
