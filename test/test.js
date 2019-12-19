998c43a3417fcc912c251a5eecceca5248638ae9
Jaid <jaid.jsx@gmail.com>
2019-12-19 20:43:06 +0100

    Added test for `--help`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -5,7 +5,15 @@ const main = path.resolve(process.env.MAIN)
 
 it("should run", async () => {
   const outputFile = path.join(__dirname, "..", "dist", "test", "readme.md")
-  return coffee.fork(main, ["output-file", outputFile])
+  return coffee.fork(main, ["--output-file", outputFile])
+    .expect("code", 0)
+    .debug()
+    .end()
+})
+
+it("should output help", async () => {
+  return coffee.fork(main, ["--help"])
+    .expect("stdout", /Options:/)
     .expect("code", 0)
     .debug()
     .end()
